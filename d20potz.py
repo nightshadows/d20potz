68cbff4fd62d0ade454c7dfcf6ccd18df63fecd7
Max Sarmanov <maxim.sarmanov@gmail.com>
2022-08-22 15:35:10 +0200

    setplayerlist and currentplayer commands

--- d20potz.py
+++ d20potz.py
@@ -33,26 +33,26 @@ logger = logging.getLogger(__name__)
 ###############################################################################################
 
 def getCurrentPlayerId(chat_id):
-    current_player_id_key = "current_player_{}".format(chat_id)
+    current_player_id_key = "current_player_{}".format(chat_id).encode('utf-8')
     current_player_id = DB.Get(current_player_id_key)
-    return current_player_id
+    return int(current_player_id.decode('utf-8'))
 
 def getPlayerById(chat_id, player_id):
-    player_list_key = "player_list_{}".format(chat_id)
-    player_list = DB.Get(player_list_key).split()
+    player_list_key = "player_list_{}".format(chat_id).encode('utf-8')
+    player_list = DB.Get(player_list_key).decode('utf-8').split()
     return player_list[player_id]
 
 def getNextPlayerId(chat_id):
-    player_list_key = "player_list_{}".format(chat_id)
-    player_list = DB.Get(player_list_key).split()
-    current_player_id_key = "current_player_{}".format(chat_id)
-    current_player_id = DB.Get(current_player_id_key)
+    player_list_key = "player_list_{}".format(chat_id).encode('utf-8')
+    player_list = DB.Get(player_list_key).decode('utf-8').split()
+    current_player_id_key = "current_player_{}".format(chat_id).encode('utf-8')
+    current_player_id = int(DB.Get(current_player_id_key).decode('utf-8'))
     next_player_id = (int(current_player_id) + 1) % len(player_list)
     return next_player_id
 
 def setCurrentPlayerId(chat_id, player_id):
-    current_player_id_key = "current_player_{}".format(chat_id)
-    DB.Put(current_player_id_key, player_id)
+    current_player_id_key = "current_player_{}".format(chat_id).encode('utf-8')
+    DB.Put(current_player_id_key, str(player_id).encode('utf-8'))
 
 ###############################################################################################
 
@@ -70,11 +70,22 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
 async def endTurn(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    currentPlayer = getPlayerById(getCurrentPlayerId(update.effective_chat.id))
+    currentPlayer = getPlayerById(update.effective_chat.id, getCurrentPlayerId(update.effective_chat.id))
     nextPlayerId = getNextPlayerId(update.effective_chat.id)
     setCurrentPlayerId(update.effective_chat.id, nextPlayerId)
-    nextPlayer = getPlayerById(nextPlayerId)
+    nextPlayer = getPlayerById(update.effective_chat.id, nextPlayerId)
     await context.bot.send_message(chat_id=update.effective_chat.id, text="{}'s turn ended. It is now {}'s turn.".format(currentPlayer, nextPlayer))
+
+async def setPlayerList(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    player_list_key = "player_list_{}".format(update.effective_chat.id).encode('utf-8')
+    player_list = ' '.join(update.message.text.split()[1:])
+    DB.Put(player_list_key, player_list.encode('utf-8'))
+    setCurrentPlayerId(update.effective_chat.id, 0)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Player list set.")
+
+async def currentPlayer(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    currentPlayer = getPlayerById(update.effective_chat.id, getCurrentPlayerId(update.effective_chat.id))
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="It is {}'s turn.".format(currentPlayer))
 
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="d20potz ready to help")
@@ -90,6 +101,12 @@ def d20potzbot():
 
     endTurn_handler = CommandHandler('endturn', endTurn)
     application.add_handler(endTurn_handler)
+
+    setPlayerList_handler = CommandHandler('setplayerlist', setPlayerList)
+    application.add_handler(setPlayerList_handler)
+
+    currentPlayer_handler = CommandHandler('currentplayer', currentPlayer)
+    application.add_handler(currentPlayer_handler)
 
     echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
     application.add_handler(echo_handler)
