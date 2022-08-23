b1837599928f2d8e51c706d897385ae0a0ff9f8d
Max Sarmanov <maxim.sarmanov@gmail.com>
2022-08-23 15:36:05 +0200

    added hp command

--- d20potz.py
+++ d20potz.py
@@ -55,6 +55,24 @@ def setCurrentPlayerId(chat_id, player_id):
     current_player_id_key = "current_player_{}".format(chat_id).encode('utf-8')
     DB.Put(current_player_id_key, str(player_id).encode('utf-8'))
 
+def getPlayerHp(chat_id, player_id):
+    player_hp_key = "player_hp_{}_{}".format(chat_id, player_id).encode('utf-8')
+    player_hp = DB.Get(player_hp_key)
+    return int(player_hp.decode('utf-8'))
+
+def getPlayerMaxHp(chat_id, player_id):
+    player_max_hp_key = "player_max_hp_{}_{}".format(chat_id, player_id).encode('utf-8')
+    player_max_hp = DB.Get(player_max_hp_key)
+    return int(player_max_hp.decode('utf-8'))
+
+def setPlayerHp(chat_id, player_id, hp):
+    player_hp_key = "player_hp_{}_{}".format(chat_id, player_id).encode('utf-8')
+    DB.Put(player_hp_key, str(hp).encode('utf-8'))
+
+def setPlayerMaxHp(chat_id, player_id, max_hp):
+    player_max_hp_key = "player_max_hp_{}_{}".format(chat_id, player_id).encode('utf-8')
+    DB.Put(player_max_hp_key, str(max_hp).encode('utf-8'))
+
 ###############################################################################################
 
 def error(bot, update, error):
@@ -91,6 +109,48 @@ async def currentPlayer(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def roll20(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Rolling... {}".format(random.randint(1,20)))
 
+async def hp(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    currentPlayer = getPlayerById(update.effective_chat.id, getCurrentPlayerId(update.effective_chat.id))
+    playerName = currentPlayer
+    subcommand = "get"
+    if len(update.message.text.split()) > 1:
+        playerName = update.message.text.split()[1]
+    if len(update.message.text.split()) > 2:
+        subcommand = update.message.text.split()[2]
+    if subcommand == "get":
+        try:
+            hp = getPlayerHp(update.effective_chat.id, playerName)
+            await context.bot.send_message(chat_id=update.effective_chat.id, text="{} has {} HP.".format(playerName, hp))
+        except:
+            await context.bot.send_message(chat_id=update.effective_chat.id, text="{} does not exist.".format(playerName))
+    elif subcommand == "set" or subcommand == "=":
+        if len(update.message.text.split()) > 3:
+            hp = int(update.message.text.split()[3])
+            setPlayerHp(update.effective_chat.id, playerName, hp)
+            setPlayerMaxHp(update.effective_chat.id, playerName, hp)
+            await context.bot.send_message(chat_id=update.effective_chat.id, text="{}'s HP set to {}.".format(playerName, hp))
+        else:
+            await context.bot.send_message(chat_id=update.effective_chat.id, text="Usage: /hp <player> = <hp>")
+    elif subcommand == "add" or subcommand == "+":
+        if len(update.message.text.split()) > 3:
+            hp = int(update.message.text.split()[3])
+            max_hp = getPlayerMaxHp(update.effective_chat.id, playerName)
+            newHp = min(getPlayerHp(update.effective_chat.id, playerName) + hp, max_hp)
+            setPlayerHp(update.effective_chat.id, playerName, newHp)
+            await context.bot.send_message(chat_id=update.effective_chat.id, text="{}'s HP set to {}.".format(playerName, newHp))
+        else:
+            await context.bot.send_message(chat_id=update.effective_chat.id, text="Usage: /hp <player> + <hp>")
+    elif subcommand == "sub" or subcommand == "-":
+        if len(update.message.text.split()) > 3:
+            hp = int(update.message.text.split()[3])
+            newHp = max(getPlayerHp(update.effective_chat.id, playerName) - hp, 0)
+            setPlayerHp(update.effective_chat.id, playerName, newHp)
+            await context.bot.send_message(chat_id=update.effective_chat.id, text="{}'s HP set to {}.".format(playerName, newHp))
+        else:
+            await context.bot.send_message(chat_id=update.effective_chat.id, text="Usage: /hp <player> - <hp>")
+    else:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="Usage: /hp <player> [get|set|=|add|+|sub|-] <hp>")
+
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="d20potz ready to help")
 
@@ -114,6 +174,9 @@ def d20potzbot():
 
     roll20_handler = CommandHandler('roll20', roll20)
     application.add_handler(roll20_handler)
+
+    hp_handler = CommandHandler('hp', hp)
+    application.add_handler(hp_handler)
 
     echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
     application.add_handler(echo_handler)
