b0fa497e48cf0bd25857f2b6d25b0201e3c324a8
Max Sarmanov <maxim.sarmanov@gmail.com>
2022-08-24 14:19:37 +0200

    added spellings and defaults values for player order and hps

--- d20potz.py
+++ d20potz.py
@@ -11,7 +11,7 @@ import random
 from telegram import Update
 from telegram.ext import filters, ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler
 
-D20PotzBotConfiguration = collections.namedtuple("D20PotzBotConfiguration", "db_location token cards_dir")
+D20PotzBotConfiguration = collections.namedtuple("D20PotzBotConfiguration", "db_location token cards_dir spelling order hp_defaults")
 def read_configuration(configuration_file):
     from configparser import ConfigParser
     cp = ConfigParser()
@@ -19,15 +19,18 @@ def read_configuration(configuration_file):
         db_location = cp.get("bot", "db_dir")
         token = cp.get("bot", "telegram_token")
         cards_dir = cp.get("bot", "cards_dir", fallback="./cards")
-        return D20PotzBotConfiguration(db_location, token, cards_dir)
+        spelling = cp.items("spelling")
+        order = cp.get("general", "player_list", fallback="")
+        hp_defaults = cp.items("hp")
+        return D20PotzBotConfiguration(db_location, token, cards_dir, spelling, order, hp_defaults)
 
 def read_cards(cards_dir):
     cards = dict()
     for subdirname in os.listdir(cards_dir):
         if os.path.isdir(os.path.join(cards_dir, subdirname)):
-            cards[subdirname] = ()
+            cards[subdirname] = list()
             for filename in os.listdir(os.path.join(cards_dir, subdirname)):
-                if filename.endswith(".JPG"):
+                if filename.lower().endswith(".jpg"):
                     cards[subdirname].append(filename[0:-4])
     return cards
 
@@ -65,23 +68,44 @@ def setCurrentPlayerId(chat_id, player_id):
     current_player_id_key = "current_player_{}".format(chat_id).encode('utf-8')
     DB.Put(current_player_id_key, str(player_id).encode('utf-8'))
 
-def getPlayerHp(chat_id, player_id):
-    player_hp_key = "player_hp_{}_{}".format(chat_id, player_id).encode('utf-8')
+def getPlayerHp(chat_id, player_name):
+    player_hp_key = "player_hp_{}_{}".format(chat_id, player_name.lower()).encode('utf-8')
     player_hp = DB.Get(player_hp_key)
     return int(player_hp.decode('utf-8'))
 
-def getPlayerMaxHp(chat_id, player_id):
-    player_max_hp_key = "player_max_hp_{}_{}".format(chat_id, player_id).encode('utf-8')
+def getPlayerMaxHp(chat_id, player_name):
+    player_max_hp_key = "player_max_hp_{}_{}".format(chat_id, player_name.lower()).encode('utf-8')
     player_max_hp = DB.Get(player_max_hp_key)
     return int(player_max_hp.decode('utf-8'))
 
-def setPlayerHp(chat_id, player_id, hp):
-    player_hp_key = "player_hp_{}_{}".format(chat_id, player_id).encode('utf-8')
+def setPlayerHp(chat_id, player_name, hp):
+    player_hp_key = "player_hp_{}_{}".format(chat_id, player_name.lower()).encode('utf-8')
     DB.Put(player_hp_key, str(hp).encode('utf-8'))
 
-def setPlayerMaxHp(chat_id, player_id, max_hp):
-    player_max_hp_key = "player_max_hp_{}_{}".format(chat_id, player_id).encode('utf-8')
+def setPlayerMaxHp(chat_id, player_name, max_hp):
+    player_max_hp_key = "player_max_hp_{}_{}".format(chat_id, player_name.lower()).encode('utf-8')
     DB.Put(player_max_hp_key, str(max_hp).encode('utf-8'))
+
+def getSpelling(hero_name):
+    hero_name = hero_name.lower()
+    for hero, spelling in CONFIG.spelling:
+        if hero == hero_name:
+            return spelling
+    return hero_name
+
+def setPlayerOrder(chat_id, player_list):
+    player_list_key = "player_list_{}".format(chat_id).encode('utf-8')
+    player_list = ' '.join(player_list)
+    DB.Put(player_list_key, player_list.encode('utf-8'))
+    setCurrentPlayerId(chat_id, 0)
+
+def setDefaultPlayerOrder(chat_id):
+    setPlayerOrder(chat_id, CONFIG.order.lower().split())
+
+def setDefaultHps(chat_id):
+    for player_name, hp in CONFIG.hp_defaults:
+        setPlayerHp(chat_id, player_name.lower(), hp)
+        setPlayerMaxHp(chat_id, player_name.lower(), hp)
 
 ###############################################################################################
 
@@ -103,18 +127,20 @@ async def endTurn(update: Update, context: ContextTypes.DEFAULT_TYPE):
     nextPlayerId = getNextPlayerId(update.effective_chat.id)
     setCurrentPlayerId(update.effective_chat.id, nextPlayerId)
     nextPlayer = getPlayerById(update.effective_chat.id, nextPlayerId)
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="{}'s turn ended. It is now {}'s turn.".format(currentPlayer, nextPlayer))
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="{}'s turn ended. It is now {}'s turn.".format(getSpelling(currentPlayer), getSpelling(nextPlayer)))
 
 async def setPlayerList(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    player_list_key = "player_list_{}".format(update.effective_chat.id).encode('utf-8')
-    player_list = ' '.join(update.message.text.split()[1:])
-    DB.Put(player_list_key, player_list.encode('utf-8'))
-    setCurrentPlayerId(update.effective_chat.id, 0)
+    setPlayerOrder(update.effective_chat.id, update.message.text.lower().split()[1:])
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Player list set.")
+
+async def setDefaults(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    setDefaultPlayerOrder(update.effective_chat.id)
+    setDefaultHps(update.effective_chat.id)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Defaults set.")
 
 async def currentPlayer(update: Update, context: ContextTypes.DEFAULT_TYPE):
     currentPlayer = getPlayerById(update.effective_chat.id, getCurrentPlayerId(update.effective_chat.id))
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="It is {}'s turn.".format(currentPlayer))
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="It is {}'s turn.".format(getSpelling(currentPlayer)))
 
 async def roll20(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Rolling... {}".format(
@@ -131,7 +157,7 @@ async def hp(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if subcommand == "get":
         try:
             hp = getPlayerHp(update.effective_chat.id, playerName)
-            await context.bot.send_message(chat_id=update.effective_chat.id, text="{} has {} HP.".format(playerName, hp))
+            await context.bot.send_message(chat_id=update.effective_chat.id, text="{} has {} HP.".format(getSpelling(playerName), hp))
         except:
             await context.bot.send_message(chat_id=update.effective_chat.id, text="{} does not exist.".format(playerName))
     elif subcommand == "set" or subcommand == "=":
@@ -139,7 +165,7 @@ async def hp(update: Update, context: ContextTypes.DEFAULT_TYPE):
             hp = int(update.message.text.split()[3])
             setPlayerHp(update.effective_chat.id, playerName, hp)
             setPlayerMaxHp(update.effective_chat.id, playerName, hp)
-            await context.bot.send_message(chat_id=update.effective_chat.id, text="{}'s HP set to {}.".format(playerName, hp))
+            await context.bot.send_message(chat_id=update.effective_chat.id, text="{}'s HP set to {}.".format(getSpelling(playerName), hp))
         else:
             await context.bot.send_message(chat_id=update.effective_chat.id, text="Usage: /hp <player> = <hp>")
     elif subcommand == "add" or subcommand == "+":
@@ -148,7 +174,7 @@ async def hp(update: Update, context: ContextTypes.DEFAULT_TYPE):
             max_hp = getPlayerMaxHp(update.effective_chat.id, playerName)
             newHp = min(getPlayerHp(update.effective_chat.id, playerName) + hp, max_hp)
             setPlayerHp(update.effective_chat.id, playerName, newHp)
-            await context.bot.send_message(chat_id=update.effective_chat.id, text="{}'s HP set to {}.".format(playerName, newHp))
+            await context.bot.send_message(chat_id=update.effective_chat.id, text="{}'s HP set to {}.".format(getSpelling(playerName), newHp))
         else:
             await context.bot.send_message(chat_id=update.effective_chat.id, text="Usage: /hp <player> + <hp>")
     elif subcommand == "sub" or subcommand == "-":
@@ -156,7 +182,7 @@ async def hp(update: Update, context: ContextTypes.DEFAULT_TYPE):
             hp = int(update.message.text.split()[3])
             newHp = max(getPlayerHp(update.effective_chat.id, playerName) - hp, 0)
             setPlayerHp(update.effective_chat.id, playerName, newHp)
-            await context.bot.send_message(chat_id=update.effective_chat.id, text="{}'s HP set to {}.".format(playerName, newHp))
+            await context.bot.send_message(chat_id=update.effective_chat.id, text="{}'s HP set to {}.".format(getSpelling(playerName), newHp))
         else:
             await context.bot.send_message(chat_id=update.effective_chat.id, text="Usage: /hp <player> - <hp>")
     else:
@@ -189,12 +215,13 @@ def d20potzbot():
     hp_handler = CommandHandler('hp', hp)
     application.add_handler(hp_handler)
 
+    defaults_handler = CommandHandler('defaults', setDefaults)
+    application.add_handler(defaults_handler)
+
     echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
     application.add_handler(echo_handler)
     
     application.run_polling()
-
-    print(CARDS)
 
 if __name__ == "__main__":
     opt, args = ParseArgs()
