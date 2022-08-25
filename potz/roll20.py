0f536f45b599d2dbb9286c90c3745887fab14394
Max Sarmanov <maxim.sarmanov@gmail.com>
2022-08-25 12:59:16 +0200

    added newline at the end

--- potz/roll20.py
+++ potz/roll20.py
@@ -6,6 +6,7 @@ import random
 from telegram import Update
 from telegram.ext import ContextTypes
 
+
 async def roll20(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
